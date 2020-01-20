7638342316e8458d8ba39ec6acf54a4ea2284ebc
zevolpato <zevolpato@outlook.com>
2020-01-20 18:38:55 +0000

    commit

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req,res) => {
-    return res.json({ message : 'Hello World!!!!'})
+    //console.log(req.query.search);
+    return res.json({ message : 'Hello  World!!!!'})
 
 })
 
+app.delete('/:id', (req,res) =>{
+    console.log(req.params)
+    return res.json({ message : 'Delete!!!!'})
+})
 app.listen(3000)
